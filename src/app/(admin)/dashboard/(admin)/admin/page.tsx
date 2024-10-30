daaa6e65dfe4fd72ebd3e1383d9dc000f9090d29
Fajar Fathurrahman <jeynihgenk25@gmail.com>
2024-10-30 15:44:14 +0700

    Memperbaiki code

--- src/app/(admin)/dashboard/(admin)/admin/page.tsx
+++ src/app/(admin)/dashboard/(admin)/admin/page.tsx
@@ -1,7 +1,9 @@
-'use client'
+"use client";
 
 import TableData from "@/components/fragments/TableData";
+import { Layout } from "@/components/modules/import";
 import useGetAdmin from "@/lib/hooks/admin/useGetAdmin";
+import Link from "next/link";
 
 const columns = [
   { key: "id", label: "ID" },
@@ -13,10 +15,19 @@ const columns = [
 
 export default function TableAdmin() {
   const { data, status } = useGetAdmin();
-  
+
   return (
-    <div>
+    <Layout.Box className="space-y-8">
+      <div>
+        <h2>Table Admin</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cumque vel
+          eos, magni illo natus ab ex fuga ratione voluptas ut?
+        </p>
+        <Link href="/dashboard/tambahAdmin">Tambah admin</Link>
+      </div>
+
       <TableData data={data} status={status} columns={columns} page="Admin" />
-    </div>
+    </Layout.Box>
   );
 }
--- src/app/(admin)/dashboard/layout.tsx
+++ src/app/(admin)/dashboard/layout.tsx
@@ -2,25 +2,25 @@
 
 import { NavbarDashboard } from "@/components/layouts/NavbarDashboard";
 import { Layout } from "@/components/modules/import";
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from "react";
+// import { useRouter } from 'next/navigation';
+// import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const router = useRouter();
-  const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(false); 
+  // const router = useRouter();
+  // const [isLoggedIn, setIsLoggedIn] = useState<boolean | null>(false); 
 
-  useEffect(() => {
-    const loggedInStatus = localStorage.getItem('isLoggedIn') === 'true';
-    setIsLoggedIn(loggedInStatus);
+  // useEffect(() => {
+  //   const loggedInStatus = localStorage.getItem('isLoggedIn') === 'true';
+  //   setIsLoggedIn(loggedInStatus);
 
-    if (!loggedInStatus) {
-      router.push('/authentication');
-    }
-  }, [router]);
+  //   if (!loggedInStatus) {
+  //     router.push('/authentication');
+  //   }
+  // }, [router]);
 
-  if (isLoggedIn === false) {
-    return null;
-  }
+  // if (isLoggedIn === false) {
+  //   return null;
+  // }
 
   return (
     <Layout.Container className="max-w-[1024px] space-y-8">
